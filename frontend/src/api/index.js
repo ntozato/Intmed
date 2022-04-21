5ca7a620b32beef4f002316a987cef459f477355
Nícolas Tozato <ntozato9@gmail.com>
2022-04-21 12:32:24 -0300

    axios rotas

---
+++ frontend/src/api/index.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const url = 'http://localhost:3001/';
+
+const login = (user) => axios.post(`${url}users/login`, user);
+
+const getEspecialidades = (token) => axios.get(
+  `${url}especialidades`,
+  { headers: { authorization: `Token ${token}` } },
+);
+
+const getMedicos = (token) => axios.get(
+  `${url}medicos`,
+  { headers: { authorization: `Token ${token}` } },
+);
+
+const getConsultas = (token) => axios.get(
+  `${url}consultas`,
+  { headers: { authorization: `Token ${token}` } },
+);
+
+const postConsultas = (token) => axios.get(
+  `${url}consultas`,
+  { headers: { authorization: `Token ${token}` } },
+);
+
+const removeConsulta = () => axios.delete(`${url}consultas/:id`);
+
+const data = {
+  login,
+  getEspecialidades,
+  getMedicos,
+  getConsultas,
+  postConsultas,
+  removeConsulta
+}
+export default data;
