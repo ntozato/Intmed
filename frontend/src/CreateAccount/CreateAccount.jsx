fa980ff2e2c50cc8996675d4861a8301e6e18472
Nícolas Tozato <ntozato9@gmail.com>
2022-04-22 12:22:53 -0300

    create-account page

--- frontend/src/CreateAccount/CreateAccount.jsx
+++ frontend/src/CreateAccount/CreateAccount.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import logo from '../../src/intmed-logo.png';
+import eye from '../../src/vector.svg';
 import './style.css';
 
 function CreateAccount() {
+  const history = useHistory();
+  const [typePassword, setTypePassword] = useState("password");
+  const [ConfirmPassword, setConfirmPassword] = useState("password");
+
+  const changePasswordType = () => {
+    if (typePassword === "password") {
+      setTypePassword("text");
+    } else {
+      setTypePassword("password");
+    };
+  };
+
+  const changeConfirmPasswordType = () => {
+    if (ConfirmPassword === "password") {
+      setConfirmPassword("text");
+    } else {
+      setConfirmPassword("password");
+    };
+  };
+
+
   return (
-    <div>Criar Conta</div>
+    <div>
+      <div className="logo-ca">
+        <img src={logo} alt="logo" className="logo-box-ca"/>
+        <h1>Medicar</h1>
+      </div>
+      <div className="create-account">
+        <h3>Crie sua conta</h3>
+      </div>
+      <form>
+        <div className="inputs-ca">
+          <input className="name-input-ca" type="text" placeholder="Nome" />
+          <input className="email-input-ca" type="text" placeholder="Email" />
+          <div className="password-input-div-ca">
+            <input className="password-input-ca" type={typePassword} placeholder="Senha" />
+            <button onClick={() => changePasswordType()} type="button" className="eye-button">
+              <img src={eye} alt="eye" className="eye" />
+            </button>
+          </div>
+          <div className="confirm-password-input-div">
+            <input className="confirm-password-input" type={ConfirmPassword} placeholder="Confirmar Senha" />
+            <button onClick={() => changeConfirmPasswordType()} type="button" className="eye-button-ca">
+              <img src={eye} alt="eye" className="eye-ca" />
+            </button>
+          </div>
+        </div>
+        <div className="buttons-ca">
+          <button className="create-btn-ca" onClick={() => history.push("/")}><b>Cancelar</b></button>
+          <button className="access-btn-ca" onClick={() => history.push("/home")}><b>Confirmar</b></button>
+        </div>
+      </form>
+    </div>
   );
 }
 
