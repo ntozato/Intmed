6197462a5d5d1130805d8b50975f17b47cec6986
Nícolas Tozato <ntozato9@gmail.com>
2022-04-23 15:16:27 -0300

    home page

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import api from '../api/index';
 import logo from '../../src/intmed-logo.png';
+import vector from '../../src/mais-vector.svg';
+import xvector from '../../src/xvector.svg';
 import './style.css';
 
 function Home() {
@@ -13,6 +15,27 @@ function Home() {
     if (response) {
       const { data } = response;
       setConsultas(data);
+    };
+  };
+
+  const renderData = () => {
+    if (consultas) {
+      return consultas.map((consulta) => {
+        return(
+          <tr>
+          <td align="left">{consulta.medico.especialidade.nome}</td>
+          <td align="left">{`Dr. ${consulta.medico.nome}`}</td>
+          <td align="left">{consulta.dia}</td>
+          <td align="left">{consulta.horario}</td>
+          <td align="right">
+            <button className="desmarcar-btn">
+              <img src={xvector} alt="vector" className="xvector" />
+              Desmarcar
+            </button>
+          </td>
+        </tr>
+        )
+      });
     };
   };
 
@@ -29,9 +52,33 @@ function Home() {
         </div>
         <div className="menu">
           <p>Usuário</p>
-          <p>Desconectar</p>
+          <a href="/">Desconectar</a>
         </div>
       </header>
+      <section>
+        <div className="info">
+          <h4>Consulta Clínica</h4>
+          <button className="new-button">
+            <img src={vector} alt="vector" className="vector"/>
+            Nova Consulta
+          </button>
+        </div>
+        <div className="table">
+          <table>
+            <thead>
+              <tr>
+                <th align="left">ESPECIALIDADE</th>
+                <th align="left">PROFISSIONAL</th>
+                <th align="left">DATA</th>
+                <th align="left">HORA</th>
+              </tr>
+            </thead>
+            <tbody>
+              {consultas && renderData()}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
   );
 }
